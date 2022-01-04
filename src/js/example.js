c0254021edfe8c3e21a587535dc390ccee0b95d7
Julia Medvedeva <medvedevajulev@gmail.com>
2022-01-04 14:52:07 +0300

    стилистические правки

--- src/js/example.js
+++ src/js/example.js
@@ -1,20 +1,20 @@
-import '../styles/index.scss'
-import json from '../data.json'
+import '../styles/index.scss';
+import json from '../data.json';
 
-const saveRes = document.createElement('div')
-const allResults = document.createElement('div')
-const list = document.createElement('ul')
-const container = document.querySelector('.container')
-const main = document.querySelector('.main')
-const result = document.querySelector('#result-div')
-const wrapperModal = document.createElement('div')
-const button = document.createElement('button')
-const modal = document.createElement('div')
+const main = document.querySelector('.main');
+const container = document.querySelector('.container');
+const saveRes = document.createElement('div');
+const allResults = document.createElement('div');
+const list = document.createElement('ul');
+const result = document.querySelector('#result-div');
+const wrapperModal = document.createElement('div');
+const button = document.createElement('button');
+const modal = document.createElement('div');
 
-allResults.id = 'allResults'
-container.prepend(saveRes)
-main.append(allResults)
-allResults.append(list)
+allResults.id = 'allResults';
+container.prepend(saveRes);
+main.append(allResults);
+allResults.append(list);
 
 const storage = {
   val1: 0,
@@ -23,19 +23,19 @@ const storage = {
   ul: document.querySelector('ul'),
 
   valSum() {
-    return +this.val1 + +this.val2
+    return +this.val1 + +this.val2;
   },
 
   init(args) {
-    this.createLoader()
-    args.forEach(({ value, id }) => storage.addItem({ value, id }))
+    this.createLoader();
+    args.forEach(({ value, id }) => storage.addItem({ value, id }));
 
-    this.deleteLoader()
+    this.deleteLoader();
   },
 
   createLoader() {
-    const loadingDiv = document.createElement('div')
-    loadingDiv.classList.add('loading')
+    const loadingDiv = document.createElement('div');
+    loadingDiv.classList.add('loading');
     loadingDiv.innerHTML = `<div class='loading-text'>
       <span class='loading-text-words'>L</span>
       <span class='loading-text-words'>O</span>
@@ -44,47 +44,47 @@ const storage = {
       <span class='loading-text-words'>I</span>
       <span class='loading-text-words'>N</span>
       <span class='loading-text-words'>G</span>
-      </div>`
+      </div>`;
 
-    main.append(loadingDiv)
+    main.append(loadingDiv);
   },
 
   deleteLoader() {
-    const loadingAnimation = document.querySelector('.loading')
-    main.removeChild(loadingAnimation)
-    return this.main
+    const loadingAnimation = document.querySelector('.loading');
+    main.removeChild(loadingAnimation);
+    return this.main;
   },
 
   generateId() {
-    return Math.floor(Math.random() * 100)
+    return Math.floor(Math.random() * 100);
   },
 
   save(key, val) {
-    this[key] = val
+    this[key] = val;
   },
 
   update(key, val) {
-    this.save(key, val)
-    this.printCurrentResult()
+    this.save(key, val);
+    this.printCurrentResult();
   },
 
   printCurrentResult() {
-    result.innerHTML = `Результат сложения: ${this.valSum()}`
+    result.innerHTML = `Результат сложения: ${this.valSum()}`;
   },
 
   generateButtonDelete() {
-    const buttonDelete = document.createElement('button')
-    buttonDelete.innerHTML = `<i class='fas fa-backspace'></i>`
-    buttonDelete.className = 'delete'
+    const buttonDelete = document.createElement('button');
+    buttonDelete.innerHTML = `<i class='fas fa-backspace'></i>`;
+    buttonDelete.className = 'delete';
 
     updateElementStyles(buttonDelete, {
       backgroundColor: 'darkmagenta',
       color: 'antiquewhite',
       border: '1px solid',
       borderRadius: '20%',
-    })
+    });
 
-    return buttonDelete
+    return buttonDelete;
   },
 
   createItem({ value, id }) {
@@ -93,21 +93,21 @@ const storage = {
       id: id || this.generateId().toString(),
       value: value || this.valSum(),
       button: this.generateButtonDelete(),
-    }
-    item.element.setAttribute('data-id', item.id)
-    item.element.innerHTML = item.value
-    item.element.appendChild(item.button)
+    };
+    item.element.setAttribute('data-id', item.id);
+    item.element.innerHTML = item.value;
+    item.element.appendChild(item.button);
     item.button.addEventListener('click', (e) => {
-      const parent = e.currentTarget.parentNode
-      this.removeItem(parent)
-      parent.remove()
-    })
+      const parent = e.currentTarget.parentNode;
+      this.removeItem(parent);
+      parent.remove();
+    });
 
-    return item
+    return item;
   },
 
   addItem(value) {
-    const item = this.createItem({ value: value.value, id: value.id })
+    const item = this.createItem({ value: value.value, id: value.id });
 
     updateElementStyles(item.element, {
       listStyle: 'none',
@@ -119,40 +119,40 @@ const storage = {
       margin: '1%',
       border: '1px solid blue',
       backgroundColor: 'goldenrod',
-    })
+    });
 
-    this.items.push({ id: item.value.id, value: item.value.value })
+    this.items.push({ id: item.value.id, value: item.value.value });
 
-    this.ul.appendChild(item.element)
+    this.ul.appendChild(item.element);
   },
 
   removeItem(itemId) {
-    const attribute = itemId.getAttribute('data-id')
+    const attribute = itemId.getAttribute('data-id');
 
-    const item = this.items.find((i) => i.id === attribute)
+    const item = this.items.find((i) => i.id === attribute);
 
     if (item) {
-      this.ul.removeChild(itemId)
-      const index = this.items.indexOf(item)
+      this.ul.removeChild(itemId);
+      const index = this.items.indexOf(item);
       if (index > -1) {
-        this.items.splice(index, 1)
+        this.items.splice(index, 1);
       }
       if (this.items.length === 0) {
-        this.createLoader()
+        this.createLoader();
       }
-      return this.items
+      return this.items;
     }
   },
 
   createModal(args) {
-    main.append(wrapperModal)
-    button.innerText = 'Ok'
-    wrapperModal.append(button, modal)
+    main.append(wrapperModal);
+    button.innerText = 'Ok';
+    wrapperModal.append(button, modal);
     button.addEventListener('click', (e) => {
-      const parent = e.currentTarget.parentNode
-      parent.remove()
-    })
-    modal.innerText = args
+      const parent = e.currentTarget.parentNode;
+      parent.remove();
+    });
+    modal.innerText = args;
     updateElementStyles(wrapperModal, {
       position: 'fixed',
       top: 0,
@@ -160,11 +160,11 @@ const storage = {
       left: 0,
       bottom: 0,
       backgroundColor: '#860d3fa6',
-    })
+    });
     updateElementStyles(button, {
       margin: '26% 0 0 68%',
       position: 'fixed',
-    })
+    });
     updateElementStyles(modal, {
       padding: '20px',
       border: '5px solid #866a0d83',
@@ -175,33 +175,33 @@ const storage = {
       boxShadow: '5px 10px 5px #a0924552',
       margin: '25% auto',
       width: '750px',
-    })
+    });
   },
 
   localStorageCustom(action, { key, entity = '' }) {
     if (key) {
       switch (action) {
         case 'set':
-          localStorage.setItem(key, entity)
-          break
+          localStorage.setItem(key, entity);
+          break;
         case 'get':
-          localStorage.getItem(key)
-          break
+          localStorage.getItem(key);
+          break;
         default:
-          this.createModal('Метод не определен')
-          break
+          this.createModal('Метод не определен');
+          break;
       }
     } else if (!key) {
-      this.createModal('Ключ не задан')
+      this.createModal('Ключ не задан');
     }
   },
-}
+};
 
 function updateElementStyles(el, propsObj, id) {
-  id && (el.id = id)
+  id && (el.id = id);
 
   for (let key in propsObj) {
-    el.style.hasOwnProperty(key) && (el.style[key] = propsObj[key])
+    el.style.hasOwnProperty(key) && (el.style[key] = propsObj[key]);
   }
 }
 
@@ -214,7 +214,7 @@ function updateElementStyles(el, propsObj, id) {
 //   }
 // }
 
-storage.localStorageCustom('get', { key: 'test', entity: 'testing' })
+storage.localStorageCustom('get', { key: 'test', entity: 'testing' });
 
 updateElementStyles(
   saveRes,
@@ -227,13 +227,13 @@ updateElementStyles(
     marginRight: '10px',
   },
   'saveRes'
-)
+);
 
 document.querySelector('#input1').addEventListener('keyup', function ({ target: { value } }) {
-  storage.update('val1', value)
-})
+  storage.update('val1', value);
+});
 document.querySelector('#input2').addEventListener('keyup', function ({ target: { value } }) {
-  storage.update('val2', value)
-})
-document.querySelector('#saveRes').addEventListener('click', () => storage.addItem(storage.valSum))
-storage.init(json)
+  storage.update('val2', value);
+});
+document.querySelector('#saveRes').addEventListener('click', () => storage.addItem(storage.valSum));
+storage.init(json);
